395ebf26e5e6182c8ee97ba84ce7b2a09591640d
José Ferrari <licoverpa321@gmail.com>
2024-03-30 17:15:11 -0300

    adding app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AgentsPage from "./pages/AgentsPage";
+import WeaponsPage from "./pages/WeaponsPage";
+import MapsPage from "./pages/MapsPage";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agents" element={<AgentsPage />} />
+        <Route path="/weapons" element={<WeaponsPage />} />
+        <Route path="/maps" element={<MapsPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AgentsPage from "./pages/AgentsPage";
-import WeaponsPage from "./pages/WeaponsPage";
-import MapsPage from "./pages/MapsPage";
-import Home from "./pages/Home";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agents" element={<AgentsPage />} />
-      <Route path="/weapons" element={<WeaponsPage />} />
-      <Route path="/maps" element={<MapsPage />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );

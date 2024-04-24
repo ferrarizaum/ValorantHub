04cda54e80b02df256b262ab1f40bb29e26180f1
José Ferrari <licoverpa321@gmail.com>
2024-04-24 18:21:18 -0300

    adding more details to details card

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const DetailsCard = ({ data }) => {
+  const { type } = useParams();
+  console.log(type);
+  if (type === "agents") {
+    return (
+      <>
+        <div>
+          <img alt="Details" src={data.displayIcon} />
+        </div>
+        <div>
+          <h1>{data.displayName}</h1>
+        </div>
+        <div>
+          <h2>{data.description}</h2>
+        </div>
+      </>
+    );
+  }
+
+  if (type === "weapons") {
+    return (
+      <>
+        <div>
+          <img alt="Details" src={data.displayIcon} />
+        </div>
+        <div>
+          <h1>{data.displayName}</h1>
+        </div>
+        <div>
+          <h2>{data.category}</h2>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <div>
-        <h1>{data.displayName}</h1>
-      </div>
-      <div>
-        <img alt="Details" src={data.displayIcon} />
+        <h1>Loading...</h1>
       </div>
     </>
   );
